641d23cd0601eb48fc0a997e5b66bad5ec3d7775
James Prevett <jp@jamespre.dev>
2026-03-23 09:26:56 -0500

    Fixed `Crime` not being a dynamic struct

--- src/globals/process_lists.ts
+++ src/globals/process_lists.ts
@@ -12,34 +12,40 @@ export enum CrimeType {
 	Lycanthropy = 6,
 }
 
-export const Crime = struct.packed('Crime', {
-	witnessNum: t.uint32,
-	crimeType: t.uint32.$type<CrimeType>(),
-	unknown: t.uint8,
-	/** The number of stolen items (e.g. if you've stolen Gold(7), it would be equals to 7). Only for thefts */
-	quantity: t.uint32,
-	/** Assigned in accordance with nextNum */
-	serialNum: t.uint32,
-	unknown2: t.uint8,
-	/** May be date of crime? Little byte is equal to day */
-	unknownDate: t.uint32,
-	/** Negative value measured from moment of crime */
-	elapsedTime: t.float32,
-	/** The killed, forced door, stolen item etc. */
-	victimID: RefID,
-	criminalID: RefID,
-	/** Only for thefts */
-	itemBaseID: RefID,
-	/** Faction, outfit etc. Only for thefts */
-	ownershipID: RefID,
-	witnessCount: vsval,
-	witnesses: array(RefID).countedBy((crime) => get_vsval(crime.witnessCount)),
-	bounty: t.uint32,
-	crimeFactionID: RefID,
-	/** 0 - active crime, 1 - it was atoned */
-	isCleared: t.uint8,
-	unknown3: t.uint16,
-});
+export const Crime = struct.packed(
+	'Crime',
+	{
+		witnessNum: t.uint32,
+		crimeType: t.uint32.$type<CrimeType>(),
+		unknown: t.uint8,
+		/** The number of stolen items (e.g. if you've stolen Gold(7), it would be equals to 7). Only for thefts */
+		quantity: t.uint32,
+		/** Assigned in accordance with nextNum */
+		serialNum: t.uint32,
+		unknown2: t.uint8,
+		/** May be date of crime? Little byte is equal to day */
+		unknownDate: t.uint32,
+		/** Negative value measured from moment of crime */
+		elapsedTime: t.float32,
+		/** The killed, forced door, stolen item etc. */
+		victimID: RefID,
+		criminalID: RefID,
+		/** Only for thefts */
+		itemBaseID: RefID,
+		/** Faction, outfit etc. Only for thefts */
+		ownershipID: RefID,
+		witnessCount: vsval,
+		witnesses: array(RefID).countedBy((crime) => get_vsval(crime.witnessCount)),
+		bounty: t.uint32,
+		crimeFactionID: RefID,
+		/** 0 - active crime, 1 - it was atoned */
+		isCleared: t.uint8,
+		unknown3: t.uint16,
+	},
+	{
+		isDynamic: true,
+	}
+);
 
 export const CrimesOfType = struct.packed(
 	'CrimesOfType',
